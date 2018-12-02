6af2bb12709fb6cd17e4ce860753d21dfb9ac59b
Cariad Eccleston <cariad@cariad.me>
2018-12-02 17:51:25 +0000

    WIP

---
+++ setup.py
@@ -0,0 +1,40 @@
+"""
+"wordpressbackup" package setup.
+"""
+
+from setuptools import setup
+
+with open('README.md', 'r') as stream:
+    LONG_DESCRIPTION = stream.read()
+
+setup(
+    author='Cariad Eccleston',
+    author_email='cariad@cariad.me',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Site Management'
+    ],
+    description='Backup and restore all your self-hosted WordPress content.',
+    extras_require={
+        'dev': [
+            'autopep8',
+            'coverage',
+            'pylint'
+        ]
+    },
+    name='wordpressbackup',
+    license='MIT',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=[
+        'wordpressbackup'
+    ],
+    url='https://github.com/cariad/py-wordpressbackup',
+    version='0.1'
+)
